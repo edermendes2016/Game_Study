7622777a5abf025e6261f118ea7dfe5cb7e487fb
edermendes2016 <ederkbb@gmail.com>
2024-06-14 18:25:49 -0300

    moviemnto do arco do skeleton

--- src/app/home/skeleton.ts
+++ src/app/home/skeleton.ts
@@ -2,18 +2,28 @@ export const loadSkeletonSprites = (scene: Phaser.Scene): void => {
     scene.load.spritesheet("skeleton", "assets/skeleton/skeleton.png", {
         frameWidth: 64,
         frameHeight: 64,
+        margin: 64,
         spacing: 1
     });
     scene.load.spritesheet("skeleton_weapon", "assets/skeleton/quiver.png", {
         frameWidth: 64,
         frameHeight: 64,
+        margin: 64,
         spacing: 1
     });
 
     scene.load.spritesheet("skeleton_bow", "assets/skeleton/bow.png", {
         frameWidth: 64,
         frameHeight: 64,
+        margin: 64,
         spacing: 1
+    });
+
+    scene.load.spritesheet("skeleton_arrow", "assets/skeleton/arrow.png", {
+        frameWidth: 64,
+        frameHeight: 64,
+        margin: 64,
+        spacing: 0
     });
 };
 
@@ -23,7 +33,7 @@ export const createSkeletonAnimations = (scene: Phaser.Scene): void => {
         key: 'skeleton_weapon',
         frames: scene.anims.generateFrameNumbers('skeleton_weapon', {
             start: 0,
-            end: 11
+            end: 11,            
         }),
         frameRate: 3,
         repeat: -1
@@ -40,6 +50,16 @@ export const createSkeletonAnimations = (scene: Phaser.Scene): void => {
     });
 
     scene.anims.create({
+        key: 'skeleton_arrow',
+        frames: scene.anims.generateFrameNumbers('skeleton_arrow', {
+            start: 0,
+            end: 11
+        }),
+        frameRate: 3,
+        repeat: -1
+    });
+
+    scene.anims.create({
         key: 'skeleton',
         frames: scene.anims.generateFrameNumbers('skeleton', {
             start: 0,
@@ -48,23 +68,25 @@ export const createSkeletonAnimations = (scene: Phaser.Scene): void => {
         frameRate: 3,
         repeat: -1
     });
-
-   
+       
 };
 
 
 export const createSkeleton = (scene: Phaser.Scene): Phaser.GameObjects.Container => {
-    const skeletonMain = scene.physics.add.sprite(0, 370, "skeleton").setScale(1);
-    
+    const skeletonMain = scene.physics.add.sprite(0, 370, "skeleton").setScale(1);    
     const skeletonBow = scene.add.sprite(0, 370, "skeleton_bow").setScale(1);
     const skeletonWeapon = scene.add.sprite(0, 370, "skeleton_weapon").setScale(1);
+    const skeletonArrow = scene.add.sprite(0, 370, "skeleton_arrow").setScale(1.15);
+   
 
     skeletonWeapon.anims.play("skeleton_weapon", true);
     skeletonMain.anims.play("skeleton", true);
     skeletonBow.anims.play("skeleton_bow", true);
+    skeletonArrow.anims.play("skeleton_arrow", true);
+    
     
 
-    const skeletonContainer = scene.add.container(60, 200, [skeletonWeapon, skeletonMain, skeletonBow]);
+    const skeletonContainer = scene.add.container(160, 200, [skeletonWeapon, skeletonMain, skeletonBow, skeletonArrow ]);
     scene.physics.world.enable(skeletonContainer);
 
     // Ajustar o corpo de física para englobar o container
